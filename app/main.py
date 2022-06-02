de871b23ea50d977d785ca0b52c59ec74ddda22c
gevpaulson <gev.paulson@yandex.ru>
2022-06-02 08:56:30 +0300

    websocket start is ready

---
+++ app/main.py
@@ -0,0 +1,26 @@
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+
+
+class EchoWebSocket(tornado.websocket.WebSocketHandler):
+    def open(self):
+        print("WebSocket opened")
+
+    def on_message(self, message):
+        self.write_message(u"You said: " + message)
+
+    def on_close(self):
+        print("WebSocket closed")
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", EchoWebSocket),
+    ])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
